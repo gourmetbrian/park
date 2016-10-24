b203b9db8790bbf84789997c33ebe4c5ad1a50a2
Brian Lane <brian@gourmetgaming.com>
2016-10-23 22:21:33 -0400

    Finished up GUI, fixed up TableView, and finished basic setup

---
+++ Image+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Image+CoreDataClass.swift
+//  Park
+//
+//  Created by Brian Lane on 10/23/16.
+//  Copyright © 2016 Brian Lane. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Image: Car {
+
+}
---
+++ Park/CustomCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomCell.swift
+//  Park
+//
+//  Created by Brian Lane on 10/23/16.
+//  Copyright © 2016 Brian Lane. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var nickname: UILabel!
+    @IBOutlet weak var licensePlate: UILabel!
+    @IBOutlet weak var defaultLabel : UILabel!
+
+
+}
